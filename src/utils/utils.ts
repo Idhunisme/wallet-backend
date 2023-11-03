f13697af017f9dcf58d5fe074829fc174f701a9e
Faidhun Niam <faidhun@gmail.com>
2023-11-03 14:15:27 +0700

    delete variabel

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,7 +6,6 @@ export function convertToEther(
   rpc: string
 ): string {
   const web3: Web3 = new Web3(rpc);
-  const kanjud = "kanjudbadag"
 
   const etherValue = web3.utils.fromWei(balance);
 
